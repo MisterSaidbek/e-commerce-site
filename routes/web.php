02b540c95d2ceac7c4419fb2c2539000872f1731
MisterSaidbek <saidbekmustafayev@gmail.com>
2020-11-13 02:55:10 +0500

    Init Commit

---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+//
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/','IndexController@index');
+
+Route::match(['get','post'],'/admin','AdminController@login');
+
+Route::group(['middleware' => ['auth']],function(){
+    Route::get('/admin/dashboard', 'AdminController@dashboard');
+    Route::get('/admin/settings', 'AdminController@settings');
+    Route::get('/admin/check-pwd','AdminController@chkPassword');
+    Route::match(['get','post'],'/admin/update-pwd','AdminController@updatePassword');
+
+
+    //Category Routes(Admin)
+    Route::match(['get','post'],'/admin/add-category','CategoryController@addCategory');
+    Route::match(['get','post'],'/admin/edit-category/{id}','CategoryController@editCategory');
+    Route::match(['get','post'],'/admin/delete-category/{id}','CategoryController@deleteCategory');
+    Route::get('/admin/view-categories','CategoryController@viewCategories');
+
+    //Products Routes
+    Route::match(['post','get'],'/admin/add-product','ProductsController@addProduct');
+    Route::match(['get','post'],'/admin/edit-product/{id}','ProductsController@editProduct');
+    Route::get('/admin/view-products','ProductsController@viewProducts');
+    Route::get('/admin/delete-product-image/{id}','ProductsController@deleteProductImage');
+    Route::get('/admin/delete-product/{id}','ProductsController@deleteProduct');
+});
+
+Route::get('/logout', 'AdminController@logout');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
